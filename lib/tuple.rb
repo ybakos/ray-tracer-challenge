3b9d73889d763a14de0ebe933b521dc16ae7867b
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-08-16 13:58:43 -0700

    [1.1] Tuple: with w=1.0 is a pint (naive).

--- lib/tuple.rb
+++ lib/tuple.rb
@@ -1,6 +1,20 @@
 class Tuple
 
+  attr_reader :x, :y, :z, :w
+
   def initialize(x, y, z, w)
+    @x = x
+    @y = y
+    @z = z
+    @w = w
+  end
+
+  def point?
+    true
+  end
+
+  def vector?
+    false
   end
 
 end
--- test/tuple_test.rb
+++ test/tuple_test.rb
@@ -7,7 +7,19 @@ describe Tuple do
 
   describe "A tuple with w=1.0" do
 
-    a = Tuple.new(4.3, -4.2, 3.1, 1.0)
+    X = 4.3
+    Y = -4.2
+    Z = 3.1
+    W = 1.0
+    a = Tuple.new(X, Y, Z, W)
+
+    it { a.x.must_equal 4.3 }
+    it { a.y.must_equal -4.2 }
+    it { a.z.must_equal 3.1 }
+    it { a.w.must_equal 1.0 }
+
+    it("is a point") { assert a.point? }
+    it("is not a vector") { refute a.vector? }
 
   end
 
